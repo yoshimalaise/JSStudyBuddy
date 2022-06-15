c029b9fc70b9ef44a63f6153179c4dc9988a6359
yoshi malaise <yoshimalaise@MacBook-Pro-van-yoshi.local>
2022-06-15 22:20:24 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.codeSchoolInABox.JSStudyBuddy',
+  appName: 'JSStudyBuddy',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
