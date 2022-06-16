4f4ddde51b17441d988def1929136dfbe1c614ce
yoshi malaise <yoshimalaise@MacBook-Pro-van-yoshi.local>
2022-06-16 10:44:30 +0200

    setting up ios and android folders

--- capacitor.config.old.ts
+++ capacitor.config.old.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.codeSchoolInABox.JSStudyBuddy',
   appName: 'JSStudyBuddy',
   webDir: 'www',
   bundledWebRuntime: false
