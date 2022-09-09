33bc2e6ab5651688ff6da9f79ba7b7938c1ef0de
yoshimalaise <yoshimalaise@users.noreply.github.com>
2022-09-09 14:56:23 +0200

    presentations wip

--- src/app/presentations/pages/present/present.component.ts
+++ src/app/presentations/pages/present/present.component.ts
@@ -1,13 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AppState } from 'src/model/app-state.interface';
+import { state } from 'src/state/state';
 
 @Component({
   selector: 'app-present',
   templateUrl: './present.component.html',
   styleUrls: ['./present.component.scss'],
 })
-export class PresentComponent implements OnInit {
+export class PresentComponent implements OnInit, AfterViewInit {
+  state: AppState;
+  remark: any;
 
-  constructor() { }
+  constructor() {
+    this.state = state;
+    this.remark = (window as any).remark;
+   }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => { 
+      const show = this.remark.create({
+        source: this.state.selectedPresentation.body
+      });
+    }, 2000);
+  }
 
   ngOnInit() {}
 
--- src/app/presentations/presentations.module.ts
+++ src/app/presentations/presentations.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { SelectPresentationComponent } from './pages/select-presentation/select-presentation.component';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { PresentComponent } from './pages/present/present.component';
 
 
 
 @NgModule({
-  declarations: [SelectPresentationComponent],
+  declarations: [SelectPresentationComponent, PresentComponent],
   imports: [
     CommonModule,
     FormsModule,
